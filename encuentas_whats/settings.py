acf7243e7ac26d09fe067a9f8f61248b64ce7f05
Your Name <bjt.mex@gmail.com>
2022-11-06 17:39:35 -0600

    agregnado setting conf

--- encuentas_whats/settings.py
+++ encuentas_whats/settings.py
@@ -38,9 +38,12 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "whatsapp_encuestas_app",
+    'django_extensions',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+     'corsheaders.middleware.CorsMiddleware',
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
