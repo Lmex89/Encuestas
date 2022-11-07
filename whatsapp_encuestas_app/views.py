195f444009563a1eb848649751903fccb24b978c
Your Name <bjt.mex@gmail.com>
2022-11-07 12:49:32 -0600

    agregando queryset para post encuestas

--- whatsapp_encuestas_app/views.py
+++ whatsapp_encuestas_app/views.py
@@ -15,13 +15,29 @@ class EncuestaListCreateView(generics.ListCreateAPIView):
     queryset = Encuesta.objects.all()
     serializer_class = EncuestaSerializer
 
+    def get_queryset(self,pk):
+        queryset = Encuesta.objects.filter(pk=pk).prefetch_related(
+            Prefetch(
+                lookup='pregunta_set',
+                queryset=Pregunta.objects.all().prefetch_related(
+                    Prefetch(
+                        lookup='opcion_set',
+                        queryset=Opcion.objects.all(),
+                        to_attr='opciones_x'
+                    )
+                ),
+                to_attr='pregunta_x'
+            )
+        ).first()
+        return  queryset
+
     def list(self, request, *args, **kwargs):
         return super().list(request, *args, **kwargs)
 
     def post(self, request, *args, **kwargs):
         response = super().create(request, *args, **kwargs)
         encuesta_id = Encuesta.objects.latest("created_at")
-        serializer = EncuestaDetailSerializer(Encuesta.objects.get(pk=encuesta_id.id))
+        serializer = EncuestaDetailSerializer(self.get_queryset(pk=encuesta_id.id))
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
